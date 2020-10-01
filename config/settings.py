fc717a1dac1342f57b0466d8ccc8e06630df9eda
Simon Yan <zj2yan@uwaterloo.ca>
2020-10-01 13:45:01 -0400

    docker files

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g$8)nu=@#!q1!+-pk*6!9*br$r^=ni3b!3&9t5=wsp0jynbuwl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
 CORS_ORIGIN_ALLOW_ALL=True
 # Application definition
